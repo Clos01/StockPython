a23291710e5d110fd65bd047b6fc0aa0072b3e45
Carlos Rivas <Cjrivas00@gmail.com>
2025-04-22 21:56:24 -0400

    i was able to fix some styling i was able to have it correct how i wanted it to be i have downloaded yfinance and been playing around with it it seems pretty cool and it was super easy to set up

--- practice.py
+++ practice.py
@@ -1,31 +1,21 @@
 import yfinance as yf
 
-
-# i want to create it where the user can input the ticker they want to know about
-# i want to create it where the user can input the ticker they want to know about
-
 def startup():
     greeting = "hello and welcome to the stock market! "
     print(greeting)
 
 
 def chooseTicker():
-    chooseTicker = input("Please type in the ticker you would like to search up for ")
-    print(chooseTicker)
+    chooseTicker = input("Please type in the ticker you would like to search up for ").lower()
+    print(f" you have chosen {chooseTicker}")
     return chooseTicker
 
 def  lookUpTicker():
     tickerChosenByUsr = chooseTicker()
     tickerSymbol = yf.Ticker(f"{tickerChosenByUsr}")
     info = tickerSymbol.info
-    # info = ickerSymbol.info
     print(info)
     return info
-
-# ticker = yf.Ticker("aapl")
-
-
-# info = ticker.info
 
 def main():
     startup()
