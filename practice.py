0d4009a3f563e20026e2c5a98be8aeb7cc7791a7
Carlos Rivas <Cjrivas00@gmail.com>
2025-04-22 22:31:52 -0400

    i have created one edge case for  no charaters

--- practice.py
+++ practice.py
@@ -1,17 +1,34 @@
 import yfinance as yf
 
 def startup():
-    greeting = "hello and welcome to the stock market! "
+    greeting = "hello and welcome to the stock market!  "
     print(greeting)
 
 
-def chooseTicker():
-    chooseTicker = input("Please type in the ticker you would like to search up for ").lower()
-    print(f" you have chosen {chooseTicker}")
-    return chooseTicker
+def tickerInput():
+    tickerInput = input("Please type in the ticker you would like to search up for:  ").lower()
+    print(f"You have chosen {tickerInput.upper()}") 
+    return tickerInput
+
+#  I want to be able to check to see if the user didnt type anything dumb 
+
+def validateTickerInput():
+    userTicketInput = tickerInput()
+    if userTicketInput == "":
+        exitProgram()
+    else:
+       return userTicketInput
+
+def exitProgram():
+    inputForExit = input("Type 'exit' to leave program:  ").lower()
+    if inputForExit == "exit":
+        return "you have left the program!"
+    else:
+        return validateTickerInput()
+
 
 def  lookUpTicker():
-    tickerChosenByUsr = chooseTicker()
+    tickerChosenByUsr = validateTickerInput()
     tickerSymbol = yf.Ticker(f"{tickerChosenByUsr}")
     info = tickerSymbol.info
     print(info)
